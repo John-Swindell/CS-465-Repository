5a7d727492214387739e760423357c9a78bf8181
John-Swindell <jswindell28@gmail.com>
2026-03-25 22:38:35 -0500

    Module 3 Baseline Completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,21 @@
+var fs = require('fs')
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* Get travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
+
+// New GET for the trips endpoint -- accidentally added from module 5
+/*
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    }
+}
+*/
 
 module.exports = {
     travel
